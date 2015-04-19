1245f389434dc1ffa328ed7adfc2fd17bd94637f
Joon Young Seo <jmseo2@illinois.edu>
2015-04-19 12:42:15 -0500

    change minor stuff

--- gamesearch/src/datacrawl/DataCrawler.java
+++ gamesearch/src/datacrawl/DataCrawler.java
@@ -38,41 +38,41 @@ public class DataCrawler extends WebCrawler {
 	
 	@Override
 	public void visit(Page page) {
-	    int docid = page.getWebURL().getDocid();
-	    String url = page.getWebURL().getURL();
-	    String domain = page.getWebURL().getDomain();
-	    String path = page.getWebURL().getPath();
-	    String subDomain = page.getWebURL().getSubDomain();
-	    String parentUrl = page.getWebURL().getParentUrl();
-	    String anchor = page.getWebURL().getAnchor();
+		int docid = page.getWebURL().getDocid();
+		String url = page.getWebURL().getURL();
+		String domain = page.getWebURL().getDomain();
+		String path = page.getWebURL().getPath();
+		String subDomain = page.getWebURL().getSubDomain();
+		String parentUrl = page.getWebURL().getParentUrl();
+		String anchor = page.getWebURL().getAnchor();
 
-	    logger.debug("Docid: " + docid);
-	    logger.info("URL: " + url);
-	    logger.debug("Domain: " + domain);
-	    logger.debug("Sub-domain: " + subDomain);
-	    logger.debug("Path: " + path);
-	    logger.debug("Parent page: " + parentUrl);
-	    logger.debug("Anchor text: " + anchor);
+		logger.debug("Docid: " + docid);
+		logger.info("URL: " + url);
+		logger.debug("Domain: " + domain);
+		logger.debug("Sub-domain: " + subDomain);
+		logger.debug("Path: " + path);
+		logger.debug("Parent page: " + parentUrl);
+		logger.debug("Anchor text: " + anchor);
 	    
-	    if (page.getParseData() instanceof HtmlParseData) {
-	      HtmlParseData htmlParseData = (HtmlParseData) page.getParseData();
-	      String text = htmlParseData.getText();
-	      String html = htmlParseData.getHtml();
-	      List<WebURL> links = htmlParseData.getOutgoingUrls();
+		if (page.getParseData() instanceof HtmlParseData) {
+			HtmlParseData htmlParseData = (HtmlParseData) page.getParseData();
+			String text = htmlParseData.getText();
+			String html = htmlParseData.getHtml();
+			List<WebURL> links = htmlParseData.getOutgoingUrls();
 
-	      logger.debug("Text length: " + text.length());
-	      logger.debug("Html length: " + html.length());
-	      logger.debug("Number of outgoing links: " + links.size());
-	    }
+			logger.debug("Text length: " + text.length());
+			logger.debug("Html length: " + html.length());
+			logger.debug("Number of outgoing links: " + links.size());
+		}
 
-	    Header[] responseHeaders = page.getFetchResponseHeaders();
-	    if (responseHeaders != null) {
-	      logger.debug("Response headers:");
-	      for (Header header : responseHeaders) {
-	        logger.debug("\t" + header.getName() + ": " + header.getValue());
-	      }
-	    }
+		Header[] responseHeaders = page.getFetchResponseHeaders();
+		if (responseHeaders != null) {
+			logger.debug("Response headers:");
+			for (Header header : responseHeaders) {
+				logger.debug("\t" + header.getName() + ": " + header.getValue());
+			}
+		}
 
-	    logger.debug("=============");
-	  }
+		logger.debug("=============");
+	}
 }
