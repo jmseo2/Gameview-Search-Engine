5e54159b3407cb7696249fe148556b203e1b06ac
Joon Young Seo <jmseo2@illinois.edu>
2015-04-20 18:32:56 -0500

    making the class users to catch exception instead

--- gamesearch/src/dataquery/QueryData.java
+++ gamesearch/src/dataquery/QueryData.java
@@ -1,22 +1,28 @@
 package dataquery;
 
+import java.io.IOException;
+
 import org.apache.lucene.document.Document;
 
 public class QueryData {
     public static void main(String[] args) {
-        String query = "dota";
-        DataQuerier querier 
-            = new DataQuerier()
-                .setIndexDirectory("../data/index/")
-                .setFieldType("title")
-                .setQuery(query)
-                .setup();
-        Document[] docs = querier.getRetrievedDocs(10);
-        for (Document doc : docs) {
-            System.out.println(doc.get("title"));
-            System.out.println(doc.get("url"));
-            System.out.println("");
+        try {
+            String query = "dota";
+            DataQuerier querier 
+                = new DataQuerier()
+                    .setIndexDirectory("../data/index/")
+                    .setFieldType("title")
+                    .setQuery(query)
+                    .setup();
+            Document[] docs = querier.getRetrievedDocs(10);
+            for (Document doc : docs) {
+                System.out.println(doc.get("title"));
+                System.out.println(doc.get("url"));
+                System.out.println("");
             
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
         }
     }
 }
