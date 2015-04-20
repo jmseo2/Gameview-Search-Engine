b6387d8021ac11776eb53c506dfdb456d66610fb
Joon Young Seo <jmseo2@illinois.edu>
2015-04-20 15:01:14 -0500

    test tab to spaces

--- gamesearch/src/datacrawl/CrawlData.java
+++ gamesearch/src/datacrawl/CrawlData.java
@@ -6,29 +6,32 @@ import edu.uci.ics.crawler4j.robotstxt.RobotstxtConfig;
 import edu.uci.ics.crawler4j.robotstxt.RobotstxtServer;
 
 public class CrawlData {
-	public static void main (String [] args) {
-		CrawlConfig config = new CrawlConfig();
-		config.setCrawlStorageFolder(DataCrawlConfigInfo.crawlStorageFolder);
-		config.setPolitenessDelay(DataCrawlConfigInfo.politenessDelay);
-		config.setMaxDepthOfCrawling(DataCrawlConfigInfo.maxDepthOfCrawling);
-		config.setMaxPagesToFetch(DataCrawlConfigInfo.maxPagesToFetch);
-		config.setIncludeBinaryContentInCrawling(DataCrawlConfigInfo.includeBinaryContentInFiltering);
-		config.setResumableCrawling(DataCrawlConfigInfo.resumableCrawling);
-		config.setIncludeHttpsPages(true);
-		
-		PageFetcher pageFetcher = new PageFetcher(config);
-		RobotstxtConfig robotstxtConfig = new RobotstxtConfig();
-		robotstxtConfig.setEnabled(false);
-		RobotstxtServer robotstxtServer = new RobotstxtServer(robotstxtConfig, pageFetcher);
-		try {
-			CrawlController controller = new CrawlController(config, pageFetcher, robotstxtServer);
-			for (String webpageRoot : DataCrawlConfigInfo.webpageRoots) {
-				controller.addSeed(webpageRoot);
-			}
-			
-			controller.start(DataCrawler.class, DataCrawlConfigInfo.numberOfCrawlers);
-		} catch (Exception e) {
-			
-		}
-	}
+    public static void main(String[] args) {
+        CrawlConfig config = new CrawlConfig();
+        config.setCrawlStorageFolder(DataCrawlConfigInfo.crawlStorageFolder);
+        config.setPolitenessDelay(DataCrawlConfigInfo.politenessDelay);
+        config.setMaxDepthOfCrawling(DataCrawlConfigInfo.maxDepthOfCrawling);
+        config.setMaxPagesToFetch(DataCrawlConfigInfo.maxPagesToFetch);
+        config.setIncludeBinaryContentInCrawling(DataCrawlConfigInfo.includeBinaryContentInFiltering);
+        config.setResumableCrawling(DataCrawlConfigInfo.resumableCrawling);
+        config.setIncludeHttpsPages(true);
+
+        PageFetcher pageFetcher = new PageFetcher(config);
+        RobotstxtConfig robotstxtConfig = new RobotstxtConfig();
+        robotstxtConfig.setEnabled(false);
+        RobotstxtServer robotstxtServer = new RobotstxtServer(robotstxtConfig,
+                pageFetcher);
+        try {
+            CrawlController controller = new CrawlController(config,
+                    pageFetcher, robotstxtServer);
+            for (String webpageRoot : DataCrawlConfigInfo.webpageRoots) {
+                controller.addSeed(webpageRoot);
+            }
+
+            controller.start(DataCrawler.class,
+                    DataCrawlConfigInfo.numberOfCrawlers);
+        } catch (Exception e) {
+
+        }
+    }
 }
--- gamesearch/src/dataindex/DataQuerier.java
+++ gamesearch/src/dataindex/DataQuerier.java
@@ -20,49 +20,49 @@ import org.apache.lucene.store.SimpleFSDirectory;
 import org.apache.lucene.util.Version;
 
 public class DataQuerier {
-	private String queryStr;
-	private String indexDir;
-	private Term [] queryTerms;
-	
-	public DataQuerier setIndexDirectory(String indexDirectory) {
-		indexDir = indexDirectory;
-		return this;
-	}
-	
-	public DataQuerier setQuery(String queryString) {
-		queryStr = queryString;
-		String [] tokens = queryStr.split(" ");
-		queryTerms = new Term[tokens.length];
-		for (int i = 0; i < tokens.length; i++) {
-			queryTerms[i] = new Term("body", tokens[i]);
-		}
-		return this;
-	}
-	
-	public Document [] getRetrievedDocs(int hitsPerPage) {
-		Document [] resDocs = new Document[hitsPerPage];
-		try {
-			// open index
-			File path = new File(indexDir);
-			Directory dir = FSDirectory.open(path.toPath());
-			IndexReader reader = DirectoryReader.open(dir);
-			IndexSearcher searcher = new IndexSearcher(reader);
-			
-			PhraseQuery query = new PhraseQuery();
-			for (int i = 0; i < queryTerms.length; i++) {
-				query.add(queryTerms[i]);
-			}
-			
-			TopDocs tops = searcher.search(query, hitsPerPage);
-			ScoreDoc [] scoreDocs = tops.scoreDocs;
-			
-			for (int i = 0; i < scoreDocs.length; i++) {
-				int docid = scoreDocs[i].doc;
-				resDocs[i] = searcher.doc(docid);
-			}
-		} catch (IOException e) {
-			e.printStackTrace();
-		}
-		return resDocs;
-	}
+    private String queryStr;
+    private String indexDir;
+    private Term[] queryTerms;
+
+    public DataQuerier setIndexDirectory(String indexDirectory) {
+        indexDir = indexDirectory;
+        return this;
+    }
+
+    public DataQuerier setQuery(String queryString) {
+        queryStr = queryString;
+        String[] tokens = queryStr.split(" ");
+        queryTerms = new Term[tokens.length];
+        for (int i = 0; i < tokens.length; i++) {
+            queryTerms[i] = new Term("body", tokens[i]);
+        }
+        return this;
+    }
+
+    public Document[] getRetrievedDocs(int hitsPerPage) {
+        Document[] resDocs = new Document[hitsPerPage];
+        try {
+            // open index
+            File path = new File(indexDir);
+            Directory dir = FSDirectory.open(path.toPath());
+            IndexReader reader = DirectoryReader.open(dir);
+            IndexSearcher searcher = new IndexSearcher(reader);
+
+            PhraseQuery query = new PhraseQuery();
+            for (int i = 0; i < queryTerms.length; i++) {
+                query.add(queryTerms[i]);
+            }
+
+            TopDocs tops = searcher.search(query, hitsPerPage);
+            ScoreDoc[] scoreDocs = tops.scoreDocs;
+
+            for (int i = 0; i < scoreDocs.length; i++) {
+                int docid = scoreDocs[i].doc;
+                resDocs[i] = searcher.doc(docid);
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        return resDocs;
+    }
 }
